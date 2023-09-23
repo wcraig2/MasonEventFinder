911c35fa9b752a57e4f25fbe1e8274b9a41c8f27
bill-baggins <billyolebaggins@gmail.com>
2023-09-23 16:02:43 -0400

    demonstration commit

--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -7,13 +7,14 @@
 	$: displayed_count.set(count);
 	$: offset = modulo($displayed_count, 1);
 
-	function modulo(n: number, m: number) {
+	function modulo(n: number, m: number): number {
 		// handle negative numbers
 		return ((n % m) + m) % m;
 	}
 </script>
 
 <div class="counter">
+
 	<button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5" />
