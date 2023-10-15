dc5e9f97a6b95378e89aaeb9c2b860139c020419
LysUltima <ebisutranslations@gmail.com>
2023-10-15 14:08:21 -0400

    fix page.svelte styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <!--Map-->
-<div style="; width:75%; height:750px; border:none; padding:none;">
+<div style="height:75%">
 	<iframe title="Embedded Google Maps" style="border:none; width:100%; height:100%; margin:0; padding:0;" align="center"
 	src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d6216.191630719143!2d-77.30680390030386!3d38.83026643606803!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1696535802736!5m2!1sen!2sus">
 	</iframe>
