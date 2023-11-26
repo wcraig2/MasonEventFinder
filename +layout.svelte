56a3af792362c402ff765fedbb8db81afb503a41
agrupac <143237917+agrupac@users.noreply.github.com>
2023-11-25 21:04:32 -0500

    Add files via upload

---
+++ +layout.svelte
@@ -0,0 +1,102 @@
+<script>
+	import { browser } from '$app/environment';
+	import { page } from '$app/stores';
+	import { webVitals } from '$lib/vitals';
+	import './style.css';
+	// import './black-blue.css';
+	import './map.css';
+	import Map from '$lib/Map.svelte';
+
+	import sun from "$lib/images/sun.png";
+	import moon from "$lib/images/moon.png";
+
+
+	/** @type {import('./$types').LayoutServerData} */
+	export let data;
+
+	$: if (browser && data?.analyticsId) {
+		webVitals({
+			path: $page.url.pathname,
+			params: $page.params,
+			analyticsId: data.analyticsId
+		});
+	}
+
+	const setTheme = (theme) => {
+		document.documentElement.dataset.theme = theme;
+		document.cookie = 'siteTheme = ${theme}';
+	};
+
+	let imagePath = sun;
+	function handleClick() {
+		if (imagePath === sun) {
+			imagePath = moon;
+			setTheme('dark');
+		} else {
+			imagePath = sun;
+			setTheme('light');
+		}
+	}
+</script>
+
+
+
+<title>GMU Entertainment Finder</title>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-2">
+			<!--Sidebar-->
+			<div class="sidebar">
+				<a href="/events" class="main-menu-item menu-item menu-item-div">Nearby Events</a>
+				<div class="menu-div">
+					<a href="about:blank" class="menu-item">Example Menu Item</a>
+				</div>
+				<div class="menu-bar" />
+				<a href="/places" class="main-menu-item menu-item menu-item-div">Nearby Places</a>
+				<div class="menu-bar" />
+				<a href="/favorites" class="main-menu-item menu-item menu-item-div">Favorites</a>
+				<div class="menu-bar" />
+				<a href="/popular" class="main-menu-item menu-item menu-item-div">Popular Events</a>
+				<div class="menu-bar" />
+				<a href="/suggestions" class="main-menu-item menu-item menu-item-div">Suggestions</a>
+				<div class="menu-bar" />
+				<a href="/profile" class="main-menu-item menu-item menu-item-div">My Profile</a>
+				<div class="menu-bar" />
+				<div class="search">
+					<form method="get" action="src\routes\search\+page.svelte">
+						<input type="text" placeholder="Search and hit enter..." name="s" class="search-form" required/>
+					</form>
+				</div>
+				<div class="toggler aligncenter">
+					<img src={imagePath} class="clickable" id="icon" on:click={handleClick} />
+				</div>
+			</div>
+		</div>
+		<div class="col-1" />
+		<!--MAIN CONTENT-->
+		<div id="content" class="col-6">
+			<!--Top Bar-->
+			<div>
+				<div class="row">
+					<div class="col-3">
+						<a href="/"><img
+							alt="mason logo"
+							style="width:100%"
+							src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/George_Mason_University_logo.svg/1024px-George_Mason_University_logo.svg.png"
+						/></a>
+					</div>
+					
+					<div class="col-9">
+						<br>
+						<h1>Entertainment Finder</h1>
+					</div>
+				</div>
+				<br>
+				<main>
+					<slot />
+				</main>
+			</div>
+			<div class="col-1" />
+		</div>
+	</div>
+</div>
