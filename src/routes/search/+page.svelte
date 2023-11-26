70e8fa370ef39903b688a16631fa230fec6e2db9
agrupac <143237917+agrupac@users.noreply.github.com>
2023-11-25 21:14:13 -0500

    Add files via upload

---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    let searchterms = new URLSearchParams(window.location.search).get("s");
+</script>
+
+
+<title>Search Results</title>
+<body style="background-color:rgb(205, 223, 243);">
+    <div style="text-align:center; padding:30px" >
+        <h2>You searched for: "<span id="searchterms"></span>"</h2>
+    </div>
+    
+    <script>
+        document.getElementById('searchterms').innerHTML = searchterms;
+    </script>
+</body>
